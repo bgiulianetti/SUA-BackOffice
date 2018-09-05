2a271af9a028601cf48a6be18c25c20a847f041c
Bruno Giulianetti <bruno.giulianetti@gmail.com>
2018-09-05 20:37:13 -0300

    agrgado de manejo en capade servicios para repositorio de productores

---
+++ SUA/Controllers/ProductorController.cs
@@ -0,0 +1,35 @@
+﻿using SUA.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SUA.Controllers
+{
+    public class ProductorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Productor(string dni)
+        {
+            ViewBag.mensaje = "";
+            ViewBag.bancos = UtilitiesAndStuff.GetBancos();
+            ViewBag.provincias = UtilitiesAndStuff.GetProvincias();
+            ViewBag.paises = UtilitiesAndStuff.GetPaises();
+            if (string.IsNullOrEmpty(dni))
+            {
+                ViewBag.accion = "Post";
+                ViewBag.titulo = "Crear Standupero";
+            }
+            else
+            {
+                ViewBag.accion = "Put";
+                ViewBag.titulo = "Editar Standupero";
+                var service = new StanduperoService();
+                var standupero = service.GetStanduperoByDni(dni);
+                return View(standupero);
+            }
+            return View();
+        }
+    }
+}
---
+++ SUA/Servicios/ProductorService.cs
@@ -0,0 +1,62 @@
+﻿using SUA.Models;
+using SUA.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SUA.Servicios
+{
+    public class ProductorService
+    {
+        public ESRepositorio Repository { get; set; }
+
+        public ProductorService()
+        {
+            var node = new UriBuilder("localhost");
+            node.Port = 9200;
+            var settings = new ESSettings(node);
+            Repository = new ESRepositorio(settings);
+        }
+
+        public List<Productor> GetProductores()
+        {
+            return Repository.GetProductores();
+        }
+
+        public Productor GetProductorByApellido(string apellido)
+        {
+            return Repository.GetProductorByApellido(apellido);
+        }
+
+        public Productor GetProductorByDni(string dni)
+        {
+            return Repository.GetProductorByDni(dni);
+        }
+
+        public string GetProductorInnerIdByDni(string dni)
+        {
+            return Repository.GetProductorInnerIdByDni(dni);
+        }
+
+        public void AddProductor(Productor productor)
+        {
+            Repository.AddProductor(productor);
+        }
+
+        public void UpdateProductor(Productor productor)
+        {
+            Repository.UpdateProductor(productor);
+        }
+
+        public void DeleteProductor(string dni)
+        {
+            Repository.DeleteProductor(dni);
+        }
+
+        public void DeleteAllProductores()
+        {
+            Repository.DeleteAllProductores();
+        }
+    }
+}
