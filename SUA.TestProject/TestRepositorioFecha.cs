7e844ffc40c02a94e2ee1fa97978c5261442ab4c
Bruno Giulianetti <bruno.giulianetti@gmail.com>
2018-09-11 10:31:06 -0300

    agregado de clase de test unitario para testing de repositorio fechas

---
+++ SUA.TestProject/TestRepositorioFecha.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SUA.Repositorios;
+
+namespace SUA.TestProject
+{
+    [TestClass]
+    public class TestRepositorioFecha
+    {
+        ESRepositorio repository;
+        ESSettings settings;
+        string index;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var node = new UriBuilder("localhost")
+            { Port = 9200 };
+            settings = new ESSettings(node);
+            index = "test_" + ESRepositorio.ContentType.fecha.ToString();
+            repository = new ESRepositorio(settings, index);
+            repository.CreateIndex();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            DeleteIndex();
+            repository = null;
+        }
+    }
+}
