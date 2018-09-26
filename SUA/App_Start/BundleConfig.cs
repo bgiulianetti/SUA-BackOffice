8681723346ec690e780f8a8e55a06368d847a106
Bruno Giulianetti <bruno.giulianetti@gmail.com>
2018-09-26 00:51:10 -0300

    borrado de basura que habia agrgado para que se vean los iconos. al final no anduvo, no se que onda. Correccion de vista de listado de salas

--- SUA/App_Start/BundleConfig.cs
+++ SUA/App_Start/BundleConfig.cs
@@ -26,9 +26,6 @@ namespace SUA
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include(
-                "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
         }
     }
 }
