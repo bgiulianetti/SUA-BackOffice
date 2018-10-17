9ac6db61d24a109e396ea07a34392751c9675ec0
Bruno Giulianetti <bruno.giulianetti@gmail.com>
2018-10-17 01:12:20 -0300

    agregado de modelo de usuariio

---
+++ SUA/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SUA.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int MailRecover { get; set; }
+        public bool CrearFechas { get; set; }
+        public bool CargaBordereaux { get; set; }
+        public bool CargaSalas { get; set; }
+        public bool VerBordereaux { get; set; }
+        public List<string> ElencosAsignados { get; set; }
+        public bool UserMaster { get; set; }
+        public bool MustChangePasswordAtNextLogin { get; set; }
+    }
+}
