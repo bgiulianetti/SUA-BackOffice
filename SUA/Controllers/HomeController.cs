1cb577a8d08b11f124400587399f6fb7b6f345e7
Bruno Giulianetti <bruno.giulianetti@gmail.com>
2018-10-28 11:40:46 -0300

    agregado de metodo par generar backup

--- SUA/Controllers/HomeController.cs
+++ SUA/Controllers/HomeController.cs
@@ -147,5 +147,26 @@ namespace SUA.Controllers
             return lista;
         }
 
+
+        [HttpGet]
+        public void GenerateBackup()
+        {
+            var entidades = new Dictionary<string, string>();
+            entidades.Add("standuperos", JsonConvert.SerializeObject(new StanduperoService().GetStanduperos()));
+            entidades.Add("productores", JsonConvert.SerializeObject(new ProductorService().GetProductores()));
+            entidades.Add("shows", JsonConvert.SerializeObject(new ShowService().GetShows()));
+            entidades.Add("fechas", JsonConvert.SerializeObject(new FechaService().GetFechas()));
+            entidades.Add("usuarios", JsonConvert.SerializeObject(new UserService().GetUsers()));
+            entidades.Add("salas", JsonConvert.SerializeObject(new SalaService().GetSalas()));
+            entidades.Add("restaurantes", JsonConvert.SerializeObject(new RestauranteService().GetRestaurantes()));
+            entidades.Add("logs", JsonConvert.SerializeObject(new LogService().GetLogs()));
+            entidades.Add("hoteles", JsonConvert.SerializeObject(new HotelService().GetHoteles()));
+
+            foreach (var entidad in entidades)
+            {
+                System.IO.File.WriteAllText(Server.MapPath("~/BackUp/" + DateTime.Now.ToString("yyyy-MM-dd") + "_" + entidad.Key + ".txt"), entidad.Value);
+            }
+        }
+
     }
 }
