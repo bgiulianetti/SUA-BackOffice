73f506d4139bdc52a8a8ae82d309d31281f3087b
Bruno Giulianetti <bruno.giulianetti@gmail.com>
2018-11-02 12:02:21 -0300

    creacion de nuevo controlador y vista para generar votacion

---
+++ SUA/Controllers/VotarCiudadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SUA.Controllers
+{
+    public class VotarCiudadController : Controller
+    {
+        [HttpGet]
+        public ActionResult Votar(string show)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Votar()
+        {
+            return View();
+        }
+
+    }
+}
